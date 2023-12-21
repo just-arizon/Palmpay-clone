ccb845020233ddab4a11479d72e224d1f13c4453
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2023-12-22 00:50:22 +0100

    Tailwind error

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './public/*.html',
+    './public/*.js',
+  ],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {},
+   plugins: [],
+ }
+
